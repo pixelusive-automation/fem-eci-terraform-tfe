3a52f9585c60ef13e418dd06b5e2a0f976941738
Vinay DG <lavishinprince@gmail.com>
2026-01-17 01:32:48 +0530

    configure map to loop through to create projects and workspace

--- main.tf
+++ main.tf
@@ -1,19 +1,31 @@
 module "project" {
-  source  = "ALT-F4-LLC/project/tfe"
-  version = "0.5.0"
+  for_each = local.project
+  source   = "ALT-F4-LLC/project/tfe"
+  version  = "0.5.0"
 
-  description       = "Example description of project"
-  name              = "fem-eci-project"
+  description       = each.value.description
+  name              = each.key
   organization_name = var.organization_name
 }
 
 module "workspace" {
-  source  = "ALT-F4-LLC/workspace/tfe"
-  version = "0.8.0"
+  for_each = local.workspace
+  source   = "ALT-F4-LLC/workspace/tfe"
+  version  = "0.8.0"
 
-  description       = "Example description of workspace"
-  execution_mode    = "local"
-  name              = "fem-eci-workspace"
+  description       = each.value.description
+  execution_mode    = each.value.execution_mode
+  name              = each.key
   organization_name = var.organization_name
-  project_id        = module.project.id
+  project_id        = each.value.project_id
+}
+
+moved {
+  from = module.workspace
+  to   = module.workspace["fem-eci-workspace"]
+}
+
+moved {
+  from = module.project
+  to   = module.project["fem-eci-project"]
 }
