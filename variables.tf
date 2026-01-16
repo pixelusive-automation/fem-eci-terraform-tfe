108994cea1797b6e9818b59e59a58f271f31653d
Vinay DG <lavishinprince@gmail.com>
2026-01-17 03:28:49 +0530

    move back to local

--- variables.tf
+++ variables.tf
@@ -7,8 +7,3 @@ variable "github_app_installation_id" {
   default = 104596172
   type    = number
 }
-
-variable "github_organization_name" {
-  default = "pixelusive-automation"
-  type    = string
-}
